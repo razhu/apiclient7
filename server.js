8fa0951075e0277999270b860643ee12ac6bd406
Ramiro <rhuayller@agetic.gob.bo>
2016-03-29 13:21:24 -0400

    cambios menores

--- server.js
+++ server.js
@@ -1,48 +1,39 @@
 var express 	= require('express');
 var app         = express();
 var Client      = require('node-rest-client').Client;
-
 var client      = new Client();
-
 // puerto
 var port = process.env.PORT || 8081; 
-
 var args1 = {
         headers: {"Content-Type": "application/json"},
 	    data: { "usuario": "admin", "contrasena": "admin" }
 };
-
-// AUTENTICACION
+// AUTENTICACION ================================================
 client.post("http://localhost:8080/api/v1/tokens", args1, function (data, response) {
     console.log(data.token);
     token = data.token;
     var args = {
-	headers: {"Content-Type": "application/json", 
-        "x-access-token": token }
-        };  
+	                headers: {"Content-Type": "application/json", "x-access-token": token }
+               };  
     obtenerdatos(args);
     //console.log(response);
 });
 
-// GET OBTENER DATOS        
+// GET OBTENER DATOS ================================================       
 function obtenerdatos(args) {
     client.get("http://localhost:8080/api/v1/usuarios", args, function (data, response) {
 
             guardardatos(data);
 });
 }
-//POST
+// GUARDAR DATOS ================================================
  function guardardatos(data) {
+     
             for(var i = 0; i<data.length; i++ ){
                 console.log(data[i].usuario)
             }
-
-  	//console.log(data);
-  	//console.log(response);
  }
-
 
 // iniciar el servidor ================================================
 app.listen(port);
 console.log('La magia esta en http://localhost:' + port);
-
